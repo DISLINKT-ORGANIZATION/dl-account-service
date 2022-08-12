5072267cf871d6308c1c3cd1cba26e984cc11a2f
verak13 <verakovacevic98@gmail.com>
2022-08-12 15:30:43 +0200

    feat: added checking if username exists in authentication filter

--- src/main/java/dislinkt/accountservice/config/JwtTokenAuthenticationFilter.java
+++ src/main/java/dislinkt/accountservice/config/JwtTokenAuthenticationFilter.java
@@ -1,10 +1,17 @@
 package dislinkt.accountservice.config;
 
 import io.jsonwebtoken.Claims;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.stereotype.Component;
 
 import dislinkt.accountservice.dtos.Authority;
 import dislinkt.accountservice.services.impl.JwtTokenProvider;
@@ -17,16 +24,17 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+@Component
 public class JwtTokenAuthenticationFilter extends OncePerRequestFilter {
 
-	private final JwtConfig jwtConfig;
-	private JwtTokenProvider tokenProvider;
+	@Value("${auth-service.address}")
+	private String authServiceAddress;
+	
+    @Autowired
+    private JwtConfig jwtConfig;
 
-	public JwtTokenAuthenticationFilter(JwtConfig jwtConfig, JwtTokenProvider tokenProvider) {
-
-		this.jwtConfig = jwtConfig;
-		this.tokenProvider = tokenProvider;
-	}
+    @Autowired
+    private JwtTokenProvider tokenProvider;
 
 	@Override
 	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
@@ -43,7 +51,15 @@ public class JwtTokenAuthenticationFilter extends OncePerRequestFilter {
 
 		if (tokenProvider.validateToken(token)) {
 			Claims claims = tokenProvider.getClaimsFromJWT(token);
+
 			String username = claims.getSubject();
+			RestTemplate restTemplate = new RestTemplate();
+			String fooResourceUrl = authServiceAddress + "/authentication/users/check-username/" + username;
+			ResponseEntity<Boolean> restTemplateResponse = restTemplate.getForEntity(fooResourceUrl, Boolean.class);
+			if (!restTemplateResponse.getBody()) {
+				throw new UsernameNotFoundException("Username not found on authentication service.");
+			}
+
 			List<String> authoritiesNames = (List<String>) claims.get("authorities");
 
 			List<Authority> authorities = new ArrayList<Authority>();
